4cb568d41565ff3b064d62abbd404404d5958be0
falkAnonymous <107887516+falkAnonymous@users.noreply.github.com>
2024-09-08 20:44:56 -0300

    Colocando Resposividade na pagina

--- Js/script.js
+++ Js/script.js
@@ -1,33 +1,40 @@
-const foneImg = document.getElementById('fone-img');
+document.addEventListener('DOMContentLoaded', function() {
+  const menuHamburguer = document.querySelector('.menu-hamburguer');
+  const menu = document.querySelector('nav ul');
 
-      foneImg.addEventListener('mousemove', (e) => {
-          const rect = foneImg.getBoundingClientRect();
-          const x = e.clientX - rect.left;
-          const y = e.clientY - rect.top;
-          const moveX = (x - rect.width / 2) / 20;
-          const moveY = (y - rect.height / 2) / 20;
-
-          foneImg.style.transform = `scale(1.1) translate(${moveX}px, ${moveY}px)`;
-      });
-
-      foneImg.addEventListener('mouseleave', () => {
-          foneImg.style.transform = 'scale(1)';
-      });
-
-
-      const testimonials = document.querySelectorAll('.testimonial');
-let currentIndex = 0;
-
-function showNextTestimonial() {
-  testimonials.forEach((testimonial, index) => {
-    testimonial.style.transform = `translateX(${(index - currentIndex) * 100}%)`;
+  menuHamburguer.addEventListener('click', function() {
+      menu.classList.toggle('show');
+      menuHamburguer.textContent = menu.classList.contains('show') ? '✖' : '☰';
   });
-  currentIndex = (currentIndex + 1) % testimonials.length;
-}
 
-setInterval(showNextTestimonial, 10000); // Muda a cada 10 segundos
+  const foneImg = document.getElementById('fone-img');
 
-// Inicializa a posição dos depoimentos
-showNextTestimonial();
+  foneImg.addEventListener('mousemove', (e) => {
+      const rect = foneImg.getBoundingClientRect();
+      const x = e.clientX - rect.left;
+      const y = e.clientY - rect.top;
+      const moveX = (x - rect.width / 2) / 20;
+      const moveY = (y - rect.height / 2) / 20;
 
-      
+      foneImg.style.transform = `scale(1.1) translate(${moveX}px, ${moveY}px)`;
+  });
+
+  foneImg.addEventListener('mouseleave', () => {
+      foneImg.style.transform = 'scale(1)';
+  });
+
+  const testimonials = document.querySelectorAll('.testimonial');
+  let currentIndex = 0;
+
+  function showNextTestimonial() {
+      testimonials.forEach((testimonial, index) => {
+          testimonial.style.transform = `translateX(${(index - currentIndex) * 100}%)`;
+      });
+      currentIndex = (currentIndex + 1) % testimonials.length;
+  }
+
+  setInterval(showNextTestimonial, 5000); // Muda a cada 5 segundos
+
+  // Inicializa a posição dos depoimentos
+  showNextTestimonial();
+});
